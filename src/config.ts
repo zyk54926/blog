2eefcb85efc5e2fdc333c2d33d58c6f47a7389af
zyk <zhangyongkuan1@huawei.com>
2024-08-04 21:52:55 +0800

    title

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
+  title: 'zyk的blog',
   subtitle: 'Demo 1Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
