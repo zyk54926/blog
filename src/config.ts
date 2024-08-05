dafaab6aef27e6ca9ae3bfa6d3864092f29a1d9d
zyk <zhangyongkuan1@huawei.com>
2024-08-05 21:15:36 +0800

    背景文字去链接

--- src/config.ts
+++ src/config.ts
@@ -9,19 +9,19 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'zyk的blog',
   subtitle: '',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'https://fuwari.vercel.app/_astro/demo-banner.DFyx781H_Z1gN7UP.webp',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
       enable: true,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      text: 'ly❤zyk',              // Credit text to be displayed
+      url: '#'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   favicon: [    // Leave this array empty to use the default favicon
